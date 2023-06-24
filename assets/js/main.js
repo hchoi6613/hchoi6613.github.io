3835fa35dbc47be42a46a9737c8cc8a0739b438b
hchoi6613 <128047660+hchoi6613@users.noreply.github.com>
2023-06-24 15:49:48 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,23 @@
 	var	$window = $(window),
 		$body = $('body');
 
+	//cursor
+	const cursor = document.querySelector('.cursor');
+
+	document.addEventListener('mousemove', e=> {
+	  cursor.setAttribute("style", "top: "+(e.pageY - scrollY)+"px; left:" +(e.pageX)+"px")
+	  //cursor.setAttribute("style", "top: "+e.pageY+"px; left: "+e.pageX+"px")
+	})
+  
+	document.addEventListener('click', () => {
+	  cursor.classList.add("expand");
+  
+	  setTimeout(() => {
+		cursor.classList.remove("expand");
+	  }, 500)
+	})
+	
+
 	// Breakpoints.
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
